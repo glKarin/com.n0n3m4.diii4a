aa571390a5537c4cc110816d5e22919894ef55d1
zhaochengke <beyondk2000@163.com>
2022-12-10 20:10:27 +0800

    script fix

--- doom3/neo/quake4_arm64.sh
+++ doom3/neo/quake4_arm64.sh
@@ -13,7 +13,7 @@ export TARGET=${ARCH}
 export API=21 # 14 # android 4.0 ice cream
 
 LIB_PATH="${TOOLCHAIN}/${ARCH}/lib ${TOOLCHAIN}/${ARCH}/lib64 ${TOOLCHAIN}/sysroot/usr/lib"
-DEFINES="-D__ANDROID_API__=${API} -I${TOOLCHAIN}/sysroot/usr/include -D_RAVEN"
+DEFINES="-D__ANDROID_API__=${API} -I${TOOLCHAIN}/sysroot/usr/include"
 
 	#BASEFLAGS='-march=armv7-a -fno-builtin-sin -fno-builtin-cos -fno-builtin-sinf -fno-builtin-cosf -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp -Wl,--no-undefined -fexceptions -frtti -I/prog/games/dante-es2/libogg/include -L/prog/games/dante-es2/libogg/lib'\
 scons \
