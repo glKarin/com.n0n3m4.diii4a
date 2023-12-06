f7e90fc96a0b6b26b59c81e8fb5dda27f1442b1e
Lubos <tridosm@gmail.com>
2023-12-06 17:49:37 +0100

    Compile fix for x86_64

--- Q3E/src/main/jni/doom3/neo/quake4/ai/AI_events.cpp
+++ Q3E/src/main/jni/doom3/neo/quake4/ai/AI_events.cpp
@@ -585,7 +585,7 @@ void idAI::Event_ThrowMoveable( void ) {
 	}
 	if ( moveable ) {
 		moveable->Unbind();
-		moveable->PostEventMS( &EV_SetOwner, 200, NULL );
+		moveable->PostEventMS( &EV_SetOwner, 200, 0 );
 	}
 }
 
@@ -609,7 +609,7 @@ void idAI::Event_ThrowAF( void ) {
 	}
 	if ( af ) {
 		af->Unbind();
-		af->PostEventMS( &EV_SetOwner, 200, NULL );
+		af->PostEventMS( &EV_SetOwner, 200, 0 );
 	}
 }
 
