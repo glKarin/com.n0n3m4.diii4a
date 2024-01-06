e24772accd5d865abb2ffca394c705ad3bb98896
zhaodengke <beyondk2000@gmail.com>
2024-01-06 14:02:32 +0800

    onscreen slider can setup clickable

--- idTech4Amm/src/main/java/com/karin/idTech4Amm/sys/Constants.java
+++ idTech4Amm/src/main/java/com/karin/idTech4Amm/sys/Constants.java
@@ -12,8 +12,8 @@ import java.util.Date;
  */
 public final class Constants
 {
-    public static final int CONST_UPDATE_RELEASE = 36;
-    public static final String CONST_RELEASE = "2023-12-31";
+    public static final int CONST_UPDATE_RELEASE = 37;
+    public static final String CONST_RELEASE = "2024-01-06";
     public static final String CONST_EMAIL = "beyondk2000@gmail.com";
     public static final String CONST_DEV = "Karin";
     public static final String CONST_CODE = "Harmattan";
@@ -28,18 +28,7 @@ public final class Constants
 	public static final String CONST_CHECK_FOR_UPDATE_URL = "https://raw.githubusercontent.com/glKarin/com.n0n3m4.diii4a/master/CHECK_FOR_UPDATE.json";
     public static final String CONST_LICENSE_URL = "https://raw.githubusercontent.com/glKarin/com.n0n3m4.diii4a/master/LICENSE";
 	public static final String[] CONST_CHANGES = {
-            "Fixed prelight shadow's shadow mapping.",
-            "Fixed EFX Reverb in Quake4.",
-            "Add translucent stencil shadow support in stencil shadow(bool cvar `harm_r_translucentStencilShadow`(default 0); float cvar `harm_r_stencilShadowAlpha` for setting transparency).",
-            "Add float cvar `harm_ui_subtitlesTextScale` control subtitles's text scale in Prey.",
-            "Support cvar `r_brightness`.",
-            "Fixed weapon projectile's scorches decals rendering in Prey(2006).",
-            "Data directory chooser support Android SAF.",
-            "New default on-screen buttons layout.",
-            "Add `Stupid Angry Bot`(a7x) mod of DOOM3 support(need DOOM3: RoE game data), game data directory named `sabot`. More view in`" + TextHelper.GenLinkText("https://www.moddb.com/downloads/sabot-alpha-7x", "SABot(a7x)") + "`.",
-            "Add `Overthinked DooM^3` mod of DOOM3 support, game data directory named `overthinked`. More view in`" + TextHelper.GenLinkText("https://www.moddb.com/mods/overthinked-doom3", "Overthinked DooM^3") + "`.",
-            "Add `Fragging Free` mod of DOOM3 support(need DOOM3: RoE game data), game data directory named `fraggingfree`. More view in`" + TextHelper.GenLinkText("https://www.moddb.com/mods/fragging-free", "Fragging Free") + "`.",
-            "Add `HeXen:Edge of Chaos` mod of DOOM3 support, game data directory named `hexeneoc`. More view in`" + TextHelper.GenLinkText("https://www.moddb.com/mods/hexen-edge-of-chaos", "HexenEOC") + "`.",
+            "On-screen slider button can setup clickable.",
 	};
 
     public static long GetBuildTimestamp()
