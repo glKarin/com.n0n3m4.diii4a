4726c9338e14fb63537deeaaea4f5d4a1fef5c6d
zhaodengke <beyondk2000@gmail.com>
2024-06-21 09:26:59 +0800

    D3/Q4: head visible on fullBodyAwareness mod

--- Q3E/src/main/jni/doom3/neo/quake4/gamesys/SysCvar.cpp
+++ Q3E/src/main/jni/doom3/neo/quake4/gamesys/SysCvar.cpp
@@ -650,4 +650,5 @@ idCVar harm_pm_fullBodyAwareness( "harm_pm_fullBodyAwareness", "0", CVAR_GAME | 
 idCVar harm_pm_fullBodyAwarenessOffset("harm_pm_fullBodyAwarenessOffset", "0 0 0", CVAR_ARCHIVE | CVAR_GAME | CVAR_NETWORKSYNC, "[Harmattan]: full-body awareness offset(<forward-offset> <side-offset> <up-offset>)");
 idCVar harm_pm_fullBodyAwarenessHeadJoint( "harm_pm_fullBodyAwarenessHeadJoint", "head_channel", CVAR_GAME | CVAR_ARCHIVE, "[Harmattan]: head joint when without head model in full-body awareness" );
 idCVar harm_pm_fullBodyAwarenessFixed( "harm_pm_fullBodyAwarenessFixed", "0", CVAR_GAME | CVAR_NETWORKSYNC | CVAR_BOOL | CVAR_ARCHIVE, "[Harmattan]: do not attach view position to head in full-body awareness" );
+idCVar harm_pm_fullBodyAwarenessHeadVisible( "harm_pm_fullBodyAwarenessHeadVisible", "0", CVAR_GAME | CVAR_NETWORKSYNC | CVAR_BOOL | CVAR_ARCHIVE, "[Harmattan]: do not suppress head in full-body awareness" );
 #endif
