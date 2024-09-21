2ca84b64b4ce0159b3913ce5d9f1d17654ee324f
zhaodengke <beyondk2000@gmail.com>
2024-09-21 13:10:29 +0800

    Quake4: fix weapon depth hack in player view

--- Q3E/src/main/jni/doom3/neo/renderer/RenderWorld_portals.cpp
+++ Q3E/src/main/jni/doom3/neo/renderer/RenderWorld_portals.cpp
@@ -679,6 +679,9 @@ void idRenderWorldLocal::AddAreaEntityRefs(int areaNum, const portalStack_t *ps)
 			continue;
 		}
 
+#ifdef _RAVEN //karin: Quake4 not use weaponDepthHack, instead of weaponDepthHackInViewID
+		entity->parms.weaponDepthHack = entity->parms.weaponDepthHackInViewID == tr.viewDef->renderView.viewID;
+#endif
 		vEnt = R_SetEntityDefViewEntity(entity);
 
 		// possibly expand the scissor rect
