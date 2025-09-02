da5f1e693c1018ef3e7e99e1da0f94da584ae541
Karin <beyondk2000@gmail.com>
2025-09-02 08:50:56 +0800

    Quake4 2025: font char y adjust offset

--- Q3E/src/main/jni/doom3/neo/ui/DeviceContext.cpp
+++ Q3E/src/main/jni/doom3/neo/ui/DeviceContext.cpp
@@ -982,6 +982,9 @@ int idDeviceContext::DrawText(float x, float y, float scale, idVec4 color, const
 				continue;
 			} else {
 				float yadj = useScale * glyph->top;
+#ifdef _RAVEN //karin: 2025 q4d font->horiBearingY - 1.0
+                yadj = yadj - 1.0f;
+#endif
 				PaintChar(x,y - yadj,glyph->imageWidth,glyph->imageHeight,useScale,glyph->s,glyph->t,glyph->s2,glyph->t2,glyph->glyph);
 
 				if (cursor == count) {
@@ -1035,6 +1038,9 @@ int idDeviceContext::DrawText(float x, float y, float scale, idVec4 color, const
                     continue;
                 } else {
                     float yadj = useScale * glyph->top;
+#ifdef _RAVEN //karin: 2025 q4d font->horiBearingY - 1.0
+                    yadj = yadj - 1.0f;
+#endif
                     PaintChar(x,y - yadj,glyph->imageWidth,glyph->imageHeight,useScale,glyph->s,glyph->t,glyph->s2,glyph->t2,glyph->glyph);
 
                     if( cursor == charIndex - 1 ) {
@@ -1326,6 +1332,9 @@ void idDeviceContext::DrawEditCursor(float x, float y, float scale)
 	float useScale = scale * useFont->glyphScale;
 	const glyphInfo_t *glyph2 = &useFont->glyphs[(overStrikeMode) ? '_' : '|'];
 	float	yadj = useScale * glyph2->top;
+#ifdef _RAVEN //karin: 2025 q4d font->horiBearingY - 1.0
+    yadj = yadj - 1.0f;
+#endif
 	PaintChar(x, y - yadj,glyph2->imageWidth,glyph2->imageHeight,useScale,glyph2->s,glyph2->t,glyph2->s2,glyph2->t2,glyph2->glyph);
 }
 
