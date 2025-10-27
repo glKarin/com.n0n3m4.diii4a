09ad70ecb9e0427216913f4eaf74c6a1eaa6c369
Karin <beyondk2000@gmail.com>
2025-10-27 17:24:32 +0800

    idTech4: update psk model example

--- Q3E/src/main/jni/doom3/neo/renderer/model/Model_psk.cpp
+++ Q3E/src/main/jni/doom3/neo/renderer/model/Model_psk.cpp
@@ -848,17 +848,17 @@ void idModelPsk::Print(void) const
          Sys_Printf("%d: " fmt "\n", i, __VA_ARGS__);                                \
     }                                    \
     Sys_Printf("\n------------------------------------------------------\n");
-    PSK_PART_PRINT(vertex, vertexes, "(%f, %f, %f)   ", vertexes[i][0], vertexes[i][1], vertexes[i][2])
-    PSK_PART_PRINT(wedge, wedges, "vertex=%u uv=(%f, %f) mat=%d   ", wedges[i].vertex_index, wedges[i].u, wedges[i].v, wedges[i].material_index)
+    //PSK_PART_PRINT(vertex, vertexes, "(%f, %f, %f)   ", vertexes[i][0], vertexes[i][1], vertexes[i][2])
+    //PSK_PART_PRINT(wedge, wedges, "vertex=%u uv=(%f, %f) mat=%d   ", wedges[i].vertex_index, wedges[i].u, wedges[i].v, wedges[i].material_index)
     PSK_PART_PRINT(face, faces, "wedge=(%u, %u, %u) mat=(%d, %d) sg=%u   ", faces[i].wedge_index1, faces[i].wedge_index2, faces[i].wedge_index3, faces[i].material_index, faces[i].aux_material_index, faces[i].smooth_group)
     PSK_PART_PRINT(material, materials, "%s   ", materials[i].name)
     PSK_PART_PRINT(weight, weights, "weight=%f parent=%d bone=%d   ", weights[i].weight, weights[i].vertex_index, weights[i].bone_index)
     PSK_PART_PRINT(bone, bones, "%s flags=%x children=%d parent=%d quat=(%f, %f, %f, %f) pos=(%f, %f, %f) length=%f size=(%f, %f, %f)   ", bones[i].name, bones[i].flags, bones[i].num_children, bones[i].parent_index, bones[i].qx, bones[i].qy, bones[i].qz, bones[i].qw, bones[i].localx, bones[i].localy, bones[i].localz, bones[i].length, bones[i].xsize, bones[i].ysize, bones[i].zsize)
-    PSK_PART_PRINT(normal, normals, "(%f, %f, %f)   ", normals[i][0], normals[i][1], normals[i][2])
-    PSK_PART_PRINT(color, colors, "(%u, %u, %u, %u)   ", colors[i].r, colors[i].g, colors[i].b, colors[i].a)
-    PSK_PART_PRINT(extra uv, uvs, "(%f, %f)   ", uvs[i][0], uvs[i][1])
-    PSK_PART_PRINT(morphInfo, morphInfos, "%s vertex count=%d   ", morphInfos[i].name, morphInfos[i].vertex_count)
-    PSK_PART_PRINT(morphData, morphDatas, "position_delta=(%f, %f, %f) tangent_z_delta=(%f, %f, %f) point index=%d   ", morphDatas[i].position_deltax, morphDatas[i].position_deltay, morphDatas[i].position_deltaz, morphDatas[i].tangent_z_deltax, morphDatas[i].tangent_z_deltay, morphDatas[i].tangent_z_deltaz, morphDatas[i].point_index)
+    //PSK_PART_PRINT(normal, normals, "(%f, %f, %f)   ", normals[i][0], normals[i][1], normals[i][2])
+    //PSK_PART_PRINT(color, colors, "(%u, %u, %u, %u)   ", colors[i].r, colors[i].g, colors[i].b, colors[i].a)
+    //PSK_PART_PRINT(extra uv, uvs, "(%f, %f)   ", uvs[i][0], uvs[i][1])
+    //PSK_PART_PRINT(morph info, morphInfos, "%s vertex count=%d   ", morphInfos[i].name, morphInfos[i].vertex_count)
+    //PSK_PART_PRINT(morph data, morphDatas, "position_delta=(%f, %f, %f) tangent_z_delta=(%f, %f, %f) point index=%d   ", morphDatas[i].position_deltax, morphDatas[i].position_deltay, morphDatas[i].position_deltaz, morphDatas[i].tangent_z_deltax, morphDatas[i].tangent_z_deltay, morphDatas[i].tangent_z_deltaz, morphDatas[i].point_index)
 
 #undef PSK_PART_PRINT
 }
