6ed381580d3fdf7ac7d9b1e507495492a5c62f67
Karin <beyondk2000@gmail.com>
2025-11-11 11:21:03 +0800

    idTech4: add git script for clone Doom3/Quake4/Prey source files only

---
+++ clone_doom3_quak4_prey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Init repository......";
+git init;
+git remote add -t master origin https://github.com/glKarin/com.n0n3m4.diii4a;
+git config core.sparsecheckout true;
+
+echo "Q3E/src/main/jni/doom3" > .git/info/sparse-checkout;
+echo "build_win_x64_doom3_quak4_prey.bat" >> .git/info/sparse-checkout;
+echo "build_win_x86_doom3_quak4_prey.bat" >> .git/info/sparse-checkout;
+echo "cmake_linux_build_doom3_quak4_prey.sh" >> .git/info/sparse-checkout;
+echo "cmake_msvc_build_doom3_quak4_prey.bat" >> .git/info/sparse-checkout;
+
+echo "Pull project......";
+git pull origin master --depth=1;
+
+echo "done";
