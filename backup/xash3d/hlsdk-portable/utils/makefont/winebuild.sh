59991e406b6a69cdca02b1bdb170960c5acbc471
Zhao <beyondk2000@gmail.com>
2026-02-27 16:05:07 +0800

    remove xash3d

--- backup/xash3d/hlsdk-portable/utils/makefont/winebuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-winegcc -g -m32 makefont.cpp ../common/wadlib.c ../common/cmdlib.c -I../common/ -I../../common/ -mwindows -mno-cygwin -lwinmm -fno-stack-protector 
--- backup/xash3d/xash3d-fwgs/3rdparty/nanogl/README.md
+++
@@ -1,3 +0,0 @@
-OpenGL 1.x to OpenGL ES 1.x wrapper by Olli Hinkka
-
-Extracted from Quake 2 port
